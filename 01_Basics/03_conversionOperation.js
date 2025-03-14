2f3e2a6c6c23a6ff54ef5c5e481b2a01eddb426c
SohamAP3119 <166409321+SohamAP3119@users.noreply.github.com>
2025-03-14 07:45:01 +0000

    conversionoOperations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+let score = "33abc"
+
+// console.log(typeof score)
+
+let valueInNumber = Number (score)
+
+// console.log(valueInNumber)
+// console.log( typeof valueInNumber)
+
+// "33" => 33 
+//"33bkdf" => NaN
+// true => 1 ; false = 0;
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+//1 => true
+//0 => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+/****************** OPERATIONS *********************** */
+
+let value = 24
+let negvalue = - value
+console.log(negvalue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+
+// console.log(2 * 2)
+// console.log(2 ** 3)
+
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + "2")
+console.log(1 + 2 + "2")
+
+console.log((3 +  4) * 5 + 4)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
