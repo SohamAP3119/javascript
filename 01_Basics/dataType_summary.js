a267d5c69c95d7fc893dd8cb149c1b6f97c9ad93
SohamAP3119 <166409321+SohamAP3119@users.noreply.github.com>
2025-03-14 09:54:54 +0000

    datatype_summary

---
+++ 01_Basics/dataType_summary.js
@@ -0,0 +1,12 @@
+// reference (non primitive)
+
+const heros =["shakiman", "Benten" , "Spiderr"]
+
+let myObj = {
+    name: "Soham"
+}
+
+const myFunction = function(){
+    console.log("Hello")
+}
+console.log(typeof heros)
