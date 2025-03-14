b3112f3d904aabf0283e87a856fe9920465e7459
SohamAP3119 <166409321+SohamAP3119@users.noreply.github.com>
2025-03-14 06:10:32 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+// alert(3 + 3) // we are using node js 
+console.log(3 + 3)
+
+console.log("soham")
+
+//notes 
+/*data types
+#primitive
+number => integer ( range - 2 to power 53)
+bigint
+string => " "
+boolean => true/false
+null => standalone value
+undeined => value not assign
+symbol => unique
+
+#object
+typeof 
+
+*/
+console.log( typeof "soham") //string
+console.log(typeof null); // object
