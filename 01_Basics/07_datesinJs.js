1e7169e7bc19953ac06dc8ebf85fc245f5762086
SohamAP3119 <166409321+SohamAP3119@users.noreply.github.com>
2025-03-17 07:59:29 +0000

    Dates

---
+++ 01_Basics/07_datesinJs.js
@@ -0,0 +1,17 @@
+//Dates
+let myDate = new Date()
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+let myCrreatedDate = new Date(2023, 0,23)
+console.log(myCrreatedDate.toDateString())
+let createdDate = new Date("01-14-2024")
+console.log(createdDate.toLocaleString ());
+
+let newDate = new Date()
+
+console.log(newDate)
+console.log(newDate.getDate())
+console.log(newDate.getMonth() + 1)
