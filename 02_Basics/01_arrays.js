f334c3934b828e030a68a74e6998b2bd65b89e4a
SohamAP3119 <166409321+SohamAP3119@users.noreply.github.com>
2025-03-17 10:19:41 +0000

    arrays1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,31 @@
+//arrays
+const myArr =[1,2,3,4,5]
+// console.log(myArr)
+const heroes = ["antman","Spider-Man","Super-Man"]
+// console.log(heroes[2]);
+
+//arrays methods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes(9))
+
+// const newArr =myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr)
+
+//slice and splice
+console.log("A", myArr)
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr)
+ 
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+
+console.log(myn2);
+
