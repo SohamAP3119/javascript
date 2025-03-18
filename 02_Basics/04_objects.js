7a3d08dc0ea77a56845979fa1eb347ef9631f210
SohamAP3119 <166409321+SohamAP3119@users.noreply.github.com>
2025-03-18 06:15:43 +0000

    object2

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,35 @@
+const tinderUser = new Object() //singleton object
+const tinderUser1 = {
+                                //literal object
+}
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = true
+// console.log(tinderUser);
+
+const regularUser ={
+    email : "some@gmail.com",
+    fullname : {
+        usefullname : {
+            firstname: "Soham",
+            lastname: "Patil"
+        }
+    }
+}
+// console.log(regularUser.fullname.usefullname.lastname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+//const obj4 ={ obj1,obj2,obj3}
+// const obj4 = Object.assign({}, obj1,obj2,obj3)
+const obj4 ={...obj1, ...obj2, ...obj3}
+console.log(obj4);
+
+console.log(tinderUser)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) 
